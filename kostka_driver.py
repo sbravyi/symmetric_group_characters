d4b49bcb1b7869fa0191f7ccadcdd98609f6d2b2
Louis Schatzki <52228856+LSchatzki@users.noreply.github.com>
2025-01-19 23:18:36 -0600

    Kostka Update
    Pushing code used to generate and plot data for MPS implementation of Kostkas.

--- kostka_driver.py
+++ kostka_driver.py
@@ -4,16 +4,61 @@ from kostka_builder import KostkaBuilder, partitions
 import random as rm
 import numpy as np
 import json
+import multiprocessing
+import signal
+import functools
 
 # Code to time and compare the mps Kostka algorithm to symmetrica
 
-path = './DATA/kostka_' # path prefix
+path = './DATA/kostka__short_' # path prefix
 
-start = 10
-stop =  35# non inclusive
+start = 38
+stop =  40# non inclusive
 step = 4
 relerr = 1e-12
-its = 100 # number of iterations per size
+its = 30 # number of iterations per size
+#max_time = 1 # seconds to timeout after
+
+def timeout(seconds=5, default=None):
+
+    def decorator(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            def handle_timeout(signum, frame):
+                return default
+
+            signal.signal(signal.SIGALRM, handle_timeout)
+            signal.alarm(seconds)
+
+            result = func(*args, **kwargs)
+
+            signal.alarm(0)
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+#@timeout(seconds=max_time, default = (-1, {}))
+def trial_mps(Mu, Pn):
+    table_mps = {}
+    t = time.time()
+    builder = KostkaBuilder(Mu, relerr)
+    for Lambda in Pn:
+        table_mps[Lambda] = builder.get_kostka(Lambda)
+    return time.time() - t, table_mps
+
+#@timeout(seconds=max_time, default = (-1, {}))    
+def trial_sage(Mu, Pn):
+    table_sage = {}
+    t = time.time()
+    for Lambda in Pn:
+        table_sage[Lambda] = symmetrica.kostka_number(Lambda, Mu)
+    return time.time()-t, table_sage
+        
 
 # collects data for size n
 def run(n):
@@ -28,35 +73,26 @@ def run(n):
         # run time trials
         for i in range(its):
             Mu = rm.choice(Pn) # random Mu
-            table_mps = {}
-            table_sage = {}
+            while len(Mu) > int(n/3):
+                Mu = rm.choice(Pn)
             
-            # run MPS algorithm
-            t = time.time()
-            builder = KostkaBuilder(Mu, relerr)
-            for Lambda in Pn:
-                table_mps[Lambda] = builder.get_kostka(Lambda)
-            elapsed_mps = time.time() - t
-            
-            # call SAGE
-            t = time.time()
-            for Lambda in Pn:
-                table_sage[Lambda] = symmetrica.kostka_number(Lambda, Mu)
-            elapsed_sage = time.time()-t
+            elapsed_mps, table_mps = trial_mps(Mu, Pn)
+            elapsed_sage, table_sage = trial_sage(Mu, Pn)
             
             # check for errors
             max_error = 0
             num_error = 0
-            for Lambda in Pn:
-                tmp = np.abs(table_mps[Lambda] - table_sage[Lambda])
-                if tmp > max_error:
-                    max_error = tmp
-                if tmp >= 0.5:
-                    num_error += 1
+            if table_mps and table_sage:
+                for Lambda in Pn:
+                    tmp = np.abs(table_mps[Lambda] - table_sage[Lambda])
+                    if tmp > max_error:
+                        max_error = tmp
+                        if tmp >= 0.5:
+                            num_error += 1
             
             # write to file
             json.dump([Mu, elapsed_mps, elapsed_sage, max_error, num_error], f)
             f.write('\n')
 
 for n in range(start, stop, step):
-    run(n)
+   run(n)
